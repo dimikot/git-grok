6969bd6a642daaa79b87f7f905cbfcdb147e2a8e
Dimi Kot <dimi.kot.code@gmail.com>
2025-05-25 18:45:02 -0700

    Add AI generation for the initial PR description

---
+++ tests/test_ai.py
@@ -0,0 +1,20 @@
+__import__("sys").dont_write_bytecode = True
+from helpers import (
+    TestCaseWithEmptyTestRepo,
+    git_add_commit,
+    git_touch,
+    run_git_grok,
+)
+from unittest import main
+
+
+class Test(TestCaseWithEmptyTestRepo):
+    def test_ai(self):
+        self.git_init_and_cd_to_test_dir(method="push.default")
+        git_touch("commit01")
+        git_add_commit("commit01")
+        run_git_grok(skip_update_existing_prs=True)
+
+
+if __name__ == "__main__":
+    main()
